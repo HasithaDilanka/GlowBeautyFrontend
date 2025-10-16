2c03891323e2a558bb3e13846b650d63b37b4a96
HasithaDilanka <hasionyxia@gmail.com>
2025-10-16 12:07:47 +0530

    GlowBeautyFrontend

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -36,8 +36,7 @@ export default function Footer() {
                             <li><Link to="#" className="text-blue-300 hover:text-white">Fragrance</Link></li>
                             <li><Link to="#" className="text-blue-300 hover:text-white">Hair Care</Link></li>
                             <li><Link to="#" className="text-blue-300 hover:text-white">Body Care</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">New Arrivals</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">Sale</Link></li>
+                          
                         </ul>
                     </div>
 
