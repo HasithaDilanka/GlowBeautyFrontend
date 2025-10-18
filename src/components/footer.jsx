716915b7c732afc7383865ba47732b621e8bbde5
HasithaDilanka <hasionyxia@gmail.com>
2025-10-18 14:03:37 +0530

    GlowBeautyFrontend

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -27,28 +27,18 @@ export default function Footer() {
                         <div className="flex space-x-4"></div>
                     </div>
 
-                    {/* Shop Links */}
-                    <div className="flex-1 min-w-[180px]">
-                        <h4 className="text-lg font-semibold mb-4 text-blue-200">Shop</h4>
-                        <ul className="space-y-2">
-                            <li><Link to="#" className="text-blue-300 hover:text-white">Skincare</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">Makeup</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">Fragrance</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">Hair Care</Link></li>
-                        </ul>
-                    </div>
+                    
 
                     {/* Customer Service */}
                     <div className="flex-1 min-w-[180px]">
                         <h4 className="text-lg font-semibold mb-4 text-blue-200">Customer Service</h4>
                         <ul className="space-y-2">
                             <li><Link to="/contactUs" className="text-blue-300 hover:text-white">Contact Us</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">Shipping Info</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">Returns & Exchanges</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">Size Guide</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">FAQ</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">Track Order</Link></li>
-                            <li><Link to="#" className="text-blue-300 hover:text-white">Gift Cards</Link></li>
+                            <li><Link to="/product" className="text-blue-300 hover:text-white">Product</Link></li>
+                            <li><Link to="/Reviews" className="text-blue-300 hover:text-white">Reviews</Link></li>
+                            <li><Link to="/cart" className="text-blue-300 hover:text-white">Cart</Link></li>
+                            <li><Link to="/contactUs" className="text-blue-300 hover:text-white">Contact Us</Link></li>
+                           
                         </ul>
                     </div>
 
@@ -61,14 +51,14 @@ export default function Footer() {
                                 <svg className="w-5 h-5 text-blue-400 mr-3" fill="currentColor" viewBox="0 0 24 24">
                                     <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z" />
                                 </svg>
-                                <span className="text-blue-200">123 Beauty Street, NY 10001</span>
+                                <span className="text-blue-200">123 Beauty Street, Colombo 03, Sri Lanka</span>
                             </div>
                             {/* Phone */}
                             <div className="flex items-center">
                                 <svg className="w-5 h-5 text-blue-400 mr-3" fill="currentColor" viewBox="0 0 24 24">
                                     <path d="M6.62 10.79a15.053 15.053 0 0 0 6.59 6.59l2.2-2.2a1 1 0 0 1 1.02-.24c1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20a1 1 0 0 1-1 1c-9.39 0-17-7.61-17-17a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1c0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
                                 </svg>
-                                <span className="text-blue-200">+1 (555) 123-4567</span>
+                                <span className="text-blue-200">+94 70 383 3096</span>
                             </div>
                             {/* Email */}
                             <div className="flex items-center">
